46518d7a8bf0fbb0ce7f339638fe3cd27e3d79c2
omer <somersari.96@gmail.com>
2023-10-03 10:43:30 +0300

    constant words added

---
+++ OcrStudyCase/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  OcrStudyCase
+//
+//  Created by Omer on 3.10.2023.
+//
+
+import Foundation
+
+final class Constants {
+    // MARK: - Init Methods
+    private init(){}
+
+    public static let shared = Constants()
+    
+    let words = ["pantene","orkid","gilette","elidor","dove","rexona"]
+}
